2e71d6b5a7872a561cce19062162ee9678ab2814
Danilo Bezerra <danilo.bezerra@fatec.sp.gov.br>
2024-10-01 19:38:18 -0300

    Add spaceship new features

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -2,14 +2,38 @@ using UnityEngine;
 
 public class Spaceship : MonoBehaviour
 {
+    private SpriteRenderer _renderer;
+    private Bounds _cameraBounds;
     public Projectile projectilePrefab;
     public float speed = 5f;
+
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        var height = Camera.main.orthographicSize * 2f;
+        var width = height * Camera.main.aspect;
+        _cameraBounds = new Bounds(Vector3.zero, new Vector3(width, height));
+    }
+
+    void LateUpdate()
+    {
+        var spriteWidth = _renderer.sprite.bounds.extents.x;
+        var spriteHeight = _renderer.sprite.bounds.extents.y;
+
+        var newPositon = transform.position;
+        newPositon.x = Mathf.Clamp(newPositon.x, _cameraBounds.min.x + spriteWidth, _cameraBounds.max.x - spriteWidth);
+        newPositon.y = Mathf.Clamp(newPositon.y, _cameraBounds.min.y + spriteHeight, _cameraBounds.max.y - spriteHeight);
+        transform.position = newPositon;
+    }
 
     void Update()
     {
         ApplyMovement();
         FireProjectile();
     }
+
+    
+
     void FireProjectile()
     {
         if (Input.GetButtonDown("Fire1")) {
@@ -21,6 +45,7 @@ public class Spaceship : MonoBehaviour
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
+
         transform.Translate(Time.deltaTime * speed * new Vector3(horizontal, vertical));
     }
 }
